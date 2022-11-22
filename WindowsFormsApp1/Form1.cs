d4f6012abe16f90992e5214f83f4d758c927db8d
katekondratiuk <117984788+katekondratiuk@users.noreply.github.com>
2022-11-22 18:35:09 +0200

    Створення форми головного меню і форми вкладок

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,28 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void таблиціБДToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stones f1 = new Stones();
+            f1.ShowDialog();
+        }
+
+        private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About f2 = new About();
+            f2.ShowDialog();
+        }
+
+        private void адмініструванняToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Admin f3 = new Admin();
+            f3.ShowDialog();
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }

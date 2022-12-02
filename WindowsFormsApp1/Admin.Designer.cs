cfa39ce97558397156a29b3036bf90201444daf2
Yurii Midianyi <loopfordev@gmail.com>
2022-12-02 17:45:55 +0200

    added authorization

--- WindowsFormsApp1/Admin.Designer.cs
+++ WindowsFormsApp1/Admin.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Admin
+    partial class LogIn
     {
         /// <summary>
         /// Required designer variable.
@@ -39,9 +39,10 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 196);
+            this.button1.Location = new System.Drawing.Point(37, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 34);
+            this.button1.Size = new System.Drawing.Size(61, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,21 +50,22 @@ namespace WindowsFormsApp1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(176, 195);
+            this.button2.Location = new System.Drawing.Point(132, 158);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 35);
+            this.button2.Size = new System.Drawing.Size(65, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(34, 40);
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.Size = new System.Drawing.Size(84, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Користувач";
             // 
@@ -71,40 +73,44 @@ namespace WindowsFormsApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(37, 104);
+            this.label2.Location = new System.Drawing.Point(28, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.Size = new System.Drawing.Size(57, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Пароль";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(176, 104);
+            this.txtPassword.Location = new System.Drawing.Point(132, 84);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.Size = new System.Drawing.Size(76, 20);
             this.txtPassword.TabIndex = 4;
             this.txtPassword.UseSystemPasswordChar = true;
             // 
             // cbxUser
             // 
             this.cbxUser.FormattingEnabled = true;
-            this.cbxUser.Location = new System.Drawing.Point(176, 40);
+            this.cbxUser.Location = new System.Drawing.Point(132, 32);
+            this.cbxUser.Margin = new System.Windows.Forms.Padding(2);
             this.cbxUser.Name = "cbxUser";
-            this.cbxUser.Size = new System.Drawing.Size(121, 24);
+            this.cbxUser.Size = new System.Drawing.Size(92, 21);
             this.cbxUser.TabIndex = 6;
             // 
-            // Admin
+            // LogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 242);
+            this.ClientSize = new System.Drawing.Size(248, 197);
             this.Controls.Add(this.cbxUser);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Admin";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "LogIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin";
             this.ResumeLayout(false);
